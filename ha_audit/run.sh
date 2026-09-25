f3917a0c8031719061c3620773f1ffee8b23529a
CTMDev <86384435+pnm1977@users.noreply.github.com>
2026-09-25 18:41:10 +0100

    Update run.sh

--- ha_audit/run.sh
+++ ha_audit/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting HA Audit v0.5.1"
+bashio::log.info "Starting HA Audit v0.6.0"
 
 if ! python3 /audit.py; then
     bashio::log.error "System audit module failed"
@@ -8,6 +8,10 @@ fi
 
 if ! python3 /config_scan.py; then
     bashio::log.error "Configuration inventory module failed"
+fi
+
+if ! python3 /quality_scan.py; then
+    bashio::log.error "Configuration quality module failed"
 fi
 
 bashio::log.info "HA Audit finished"
