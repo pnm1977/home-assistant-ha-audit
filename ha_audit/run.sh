2bf34927eb4f17e10855993cb63250687169da46
CTMDev <86384435+pnm1977@users.noreply.github.com>
2026-09-25 18:56:44 +0100

    Update run.sh

--- ha_audit/run.sh
+++ ha_audit/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting HA Audit v0.6.0"
+bashio::log.info "Starting HA Audit v0.6.1"
 
 if ! python3 /audit.py; then
     bashio::log.error "System audit module failed"
